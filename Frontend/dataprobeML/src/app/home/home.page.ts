64dcaefd4c0b4b75c7a10ba420f65862eaed16be
MikeClngl <michele.colangelo02@gmail.com>
2024-03-08 18:49:15 +0100

    update Homepage

--- Frontend/dataprobeML/src/app/home/home.page.ts
+++ Frontend/dataprobeML/src/app/home/home.page.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  fileIsInsert: boolean = false;
+  fileName: string | undefined;
+  fileType: string | undefined;
+  checkboxValues = {
+    bleu: false,
+    codebleu: false,
+    crystalbleu: false
+  };
 
+  //Function to manage file uploading
+  handleFileInput(event: any){
+    const file = event.target.files[0];
+
+    if (file) {
+      this.fileIsInsert = true;
+      this.fileName = file.name;
+      this.fileType = file.type;
+    }
+  }
+
+  //Function to see if almost one checkbox is selected for button "submit"
+  checkboxSelected(): boolean {
+    return this.checkboxValues.bleu || this.checkboxValues.codebleu || this.checkboxValues.crystalbleu;
+  }
 }
