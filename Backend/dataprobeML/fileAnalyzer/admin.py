815b5c0016a54e9ef9ef095eba5d35318aa32cd5
MikeClngl <michele.colangelo02@gmail.com>
2024-03-12 12:54:00 +0100

    create ReviewModel

--- Backend/dataprobeML/fileAnalyzer/admin.py
+++ Backend/dataprobeML/fileAnalyzer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from fileAnalyzer.models import ReviewModel
 
 # Register your models here.
+class ReviewModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    
+admin.site.register(ReviewModel, ReviewModelAdmin)
--- Backend/dataprobeML/fileAnalyzer/models.py
+++ Backend/dataprobeML/fileAnalyzer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ReviewModel(models.Model):
+    review = models.FileField(upload_to='reviews/')  # "reviews/" directory where files are saved
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
