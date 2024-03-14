98b787a369c7689f4a0c6d8bcfda7c308d2841c8
MikeClngl <michele.colangelo02@gmail.com>
2024-03-14 15:51:18 +0100

    create view for review Model

--- Backend/dataprobeML/fileAnalyzer/views.py
+++ Backend/dataprobeML/fileAnalyzer/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from fileAnalyzer.models import ReviewModel
+
+def create_review(request):
+    if request.method == 'POST':
+        review_name = request.POST.get('reviewName')
+        if review_name:
+            review = ReviewModel(name = review_name)
+            review.save()
+            return JsonResponse({'message': 'Review created.'}, status=201)
+        else:
+            return JsonResponse({'error': 'Name of review empty.'}, status=400)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
