cef03a5d1a59a0c5d996e82696c4e0ac003a1b91
MikeClngl <michele.colangelo02@gmail.com>
2024-08-12 12:50:02 +0200

    changed library for BLEU score

--- Backend/dataprobeML/fileAnalyzer/services.py
+++ Backend/dataprobeML/fileAnalyzer/services.py
@@ -1,12 +1,9 @@
-import nltk
-from nltk.translate.bleu_score import corpus_bleu
+import sacrebleu
 import pandas as pd
 
 def calculate_bleu_score(reference_texts, candidate_text):
-    reference_tokens = [[nltk.word_tokenize(ref.lower()) for ref in reference_texts]]
-    candidate_tokens = nltk.word_tokenize(candidate_text.lower())
-    score = corpus_bleu(reference_tokens, [candidate_tokens])
-    return score
+    score = sacrebleu.sentence_bleu(candidate_text, reference_texts)
+    return score.score
 
 def calculate_bleu_from_csv(file_path):
     df = pd.read_csv(file_path)
@@ -17,21 +14,17 @@ def calculate_bleu_from_csv(file_path):
         reference_texts = [row['reference']]
         candidate_text = row['candidate']
         try:
-            reference_tokens = [nltk.word_tokenize(ref.lower()) for ref in reference_texts]
-            candidate_tokens = nltk.word_tokenize(candidate_text.lower())
-            references.append(reference_tokens)
-            candidates.append(candidate_tokens)
+            references.append(reference_texts)
+            candidates.append(candidate_text)
         except Exception as e:
             print(f"Errore nel calcolo del punteggio BLEU per la riga: {row}")
             print(f"Errore: {e}")
 
-    # Calcolare il BLEU score complessivo usando corpus_bleu
-    overall_bleu_score = corpus_bleu(references, candidates)
-    return overall_bleu_score
+    overall_bleu_score = sacrebleu.corpus_bleu(candidates, [list(ref) for ref in zip(*references)])
+    return overall_bleu_score.score
 
-def calculate_crystal_bleu(file_path):     # TODO
+def calculate_crystal_bleu(file_path):  # TODO
     return {"crystal_bleu_score": 0}
 
-def calculate_code_bleu(file_path):    # TODO
-
+def calculate_code_bleu(file_path):  # TODO
     return {"code_bleu_score": 0}
