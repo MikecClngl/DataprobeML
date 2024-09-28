1e11a78b21ca03b6d3265902a6ba00c21334ddbf
MikeClngl <michele.colangelo02@gmail.com>
2024-09-28 19:40:45 +0200

    implemented delete function

--- Backend/dataprobeML/fileAnalyzer/admin.py
+++ Backend/dataprobeML/fileAnalyzer/admin.py
@@ -3,6 +3,6 @@ from fileAnalyzer.models import Review
 
 # Register your models here.
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'date', 'review' , 'reviewModes', 'bleuScore', 'crystalBleuScore', 'codeBleuScore']
+    list_display = ['id', 'name', 'description', 'date', 'review' , 'reviewModes', 'bleuScore', 'crystalBleuScore', 'codeBleuScore']
     
 admin.site.register(Review, ReviewAdmin)
--- Backend/dataprobeML/fileAnalyzer/urls.py
+++ Backend/dataprobeML/fileAnalyzer/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from fileAnalyzer import views
 
 urlpatterns = [
-    re_path(r'^reviews/$', views.reviewApi)
+    re_path(r'^reviews/$', views.reviewApi),
+    re_path(r'^reviews/(?P<pk>[0-9]+)$', views.reviewApi),
 ]
