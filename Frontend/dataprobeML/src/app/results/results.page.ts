214ba5e672dabb37ec93fb6ef4a3fa55be16f2ac
MikeClngl <michele.colangelo02@gmail.com>
2024-10-08 20:01:58 +0200

    managed errors in results page

--- Frontend/dataprobeML/src/app/results/results.page.ts
+++ Frontend/dataprobeML/src/app/results/results.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { Title } from '@angular/platform-browser';
+import { ReviewService } from '../services/review.service';
 
 import { ResultsService } from '../services/results.service';
 
@@ -11,13 +12,15 @@ import { ResultsService } from '../services/results.service';
   styleUrls: ['./results.page.scss'],
 })
 export class ResultsPage implements OnInit {
-  results : any
+  results : any;
+  analyses: any[] = [];
 
   constructor(
     private router: Router,
     private resultsService : ResultsService,
     private alertController: AlertController,
-    private title: Title
+    private title: Title,
+    private reviewService: ReviewService
   ) {
     title.setTitle("DataprobeML- Results")
   }
@@ -28,26 +31,109 @@ export class ResultsPage implements OnInit {
       console.log(this.results)
     } else {
       this.results = this.resultsService.getResults();
-      if (this.results.errors && this.results.errors.length > 0) {
-        this.presentAlert();
+      }
+
+      this.filterAnalyses();
+      console.log(this.analyses)
+
+      if (this.analyses.length === 0) {
+        this.presentAlert(true); // Assign true for activate "back Home button"
+      } else if (this.results.errors && this.results.errors.length > 0) {
+        this.presentAlert(false);
+    }
+  }
+
+  filterAnalyses() {
+    if (this.results) {
+      const { bleuScore, crystalBleuScore, codeBleuScore } = this.results;
+
+      if (bleuScore !== -1 && bleuScore !== 0) {
+        this.analyses.push({ type: 'BLEU', score: bleuScore });
+      }
+
+      if (crystalBleuScore !== -1 && crystalBleuScore !== 0) {
+        this.analyses.push({ type: 'CrystalBLEU', score: crystalBleuScore });
+      }
+
+      if (codeBleuScore !== -1 && codeBleuScore !== 0) {
+        this.analyses.push({ type: 'CodeBLEU', score: codeBleuScore });
       }
     }
   }
 
-  async presentAlert() {
-    const errorMessages = this.results.errors.map((error: any) => `${error.type}: ${error.error.replace(/\(<unknown>, line \d+\)/, '')}, in row: ${error.row}`).join('\n');
-    const alert = await this.alertController.create({
-      header: 'Ops! There are some errors',
-      message: errorMessages,
-      cssClass: 'custom-alert',
-      buttons: [
-        {
-          text: 'OK',
-          cssClass: 'alert-button-blue',
-        },
-      ],
+  async presentAlert(showHomeButton: boolean) {
+    const maxVisibleErrors = 10; // Max visibe errors
+    let showAll = false; //show more
+
+    const getErrorMessages = () => {
+      const visibleErrors = showAll ? this.results.errors : this.results.errors.slice(0, maxVisibleErrors);
+      const errorMessages = visibleErrors.map((error: any) =>
+        `${error.type}: ${error.error.replace(/\(<unknown>, line \d+\)/, '')}, in row: ${error.row}`
+      ).join('\n');
+
+      if (this.results.errors.length > maxVisibleErrors && !showAll) {
+        return errorMessages + `\n\n...and ${this.results.errors.length - maxVisibleErrors} more errors`;
+      }
+
+      return errorMessages;
+    };
+
+    // function to create errors alert
+    const createAlert = async () => {
+      const alertButtons: any[] = [];
+
+    if (this.results.errors.length > maxVisibleErrors) {
+    alertButtons.push({
+      text: showAll ? 'Show Less' : 'Show More',
+      cssClass: 'alert-button-blue',
+      handler: () => {
+        showAll = !showAll; // Invert "Show More" and "Show Less"
+        alert.dismiss().then(() => createAlert()); // Recreate alert
+      },
     });
-    await alert.present();
+  }
+
+    if(this.analyses.length > 0){
+      alertButtons.push({
+        text: 'OK',
+        cssClass: 'alert-button-blue',
+        role: 'cancel'
+      });
+    }
+
+      //If there are no results, add "back home" button
+      if (showHomeButton) {
+        alertButtons.push({
+          text: 'Back Home',
+          cssClass: 'alert-button-blue',
+          handler: () => {
+            this.navigateToHome();
+          },
+        });
+      }
+
+      const alert = await this.alertController.create({
+        header: 'Ops! There are some errors',
+        message: getErrorMessages(),
+        cssClass: `custom-alert ${showAll ? 'scrollable-alert' : ''}`,
+        buttons: alertButtons,
+      });
+
+      await alert.present();
+    };
+    await createAlert();
+  }
+
+  deleteReview(reviewId: number) {
+    this.reviewService.deleteReview(reviewId).subscribe(
+      response => {
+        console.log('Review deleted succesfully', response);
+        this.navigateToHome();
+      },
+      error => {
+        console.error('Error, review not deleted', error);
+      }
+    );
   }
 
   navigateToHome(){
